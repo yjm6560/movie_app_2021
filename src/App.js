60460897ee2fd3c1585e758e065895d645c6c248
unknown <Administrator@SKCC21N00373.SKCC.NET>
2021-07-12 15:31:41 +0900

    04-4 학습 내용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map(food => <Food key={food.id} name={food.name} image={food.image} rating={food.rating} />)};
     </div>
-  );
+  )
 }
+const foodLike = [
+  {
+    id      : 0,
+    name    : 'Kimchi',
+    image   : 'https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg',
+    rating  : 5.0,
+  },
+  {
+    id      : 1,
+    name    : 'Potato',
+    image   : 'https://ctftime.org/media/team/save.jpg',
+    rating  : 4.0,
+  },
+]
+
+function Food({name, image, rating}){
+    return (
+      <div>
+        <h3>I love {name}</h3>
+        <h4>Rating : {rating}/5.0</h4>
+        <img src={image} style={{width : 300 + "px"}} alt={name}></img>
+      </div>
+    );
+}
+
+Food.propTypes = {
+  name    : PropTypes.string.isRequired,
+  image   : PropTypes.string.isRequired,
+  rating  : PropTypes.number.isRequired,
+};
 
 export default App;
